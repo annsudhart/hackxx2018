447f61f5efeb3c84633d4e9d1cfa0a37fd9c78e1
annsudhart <sudharta.andrea@gmail.com>
2018-04-08 13:09:11 -0700

    Added some messages of encouragement :)

--- backend/Encouragement.java
+++ backend/Encouragement.java
@@ -1,3 +1,19 @@
 public class Encouragement {
+	private final String HYPATIA = "Hypatia c.370 - 415.  She worked " +
+	"to teach and preserve Greek mathematics in her time. She was also " +
+	"known for her strength in philosophy and reasoning.";
+	private final String MARYAM = "Maryam Mirzakhani 1977-2017. She won " +
+	"the prestigious Fields medal in Mathematics for her research in " +
+	"geometry, and was the first woman to win such a medal.";
+	private final String AGNESI;
+	private String[] encouragement="";
+	public Encouragement() {
 
+	}
+
+	// generate a specific String for encouragement
+	public String generateEncouragement(int x) {
+	}
+
+	// generate a random String for encouragement
 }
